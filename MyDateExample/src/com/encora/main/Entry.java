0f180fe3e1aa3cd5821440d41c55c5cae74b115a
Nagabhusan <nagabusanrao@gmail.com>
2023-04-26 11:59:51 +0530

    date example added

---
+++ MyDateExample/src/com/encora/main/Entry.java
@@ -0,0 +1,12 @@
+package com.encora.main;
+import com.encora.util.MyDate;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		MyDate d = new MyDate(-3,11,2031);
+		
+		System.out.println(d);
+	}
+
+}
---
+++ MyDateExample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nagabhusana.bommi
+ *
+ */
+module MyDateExample {
+}
